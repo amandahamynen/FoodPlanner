79b6ead7bfbd870fb0e52e03112369663a0e46c1
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2025-09-05 13:03:46 +0300

    Create template for viewing food items

--- foodplanner-frontend/src/App.tsx
+++ foodplanner-frontend/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface FoodItem {
+  id: number,
+  name: string
+}
+
 function App() {
+  const [foodItems, setFoodItems] = useState<FoodItem[]>([{id: 1, name: 'Apple'}, {id: 2, name: 'Banana'}]);
+  const [newFoodItem, setNewFoodItem] = useState<string>('');
+
+  const foodItemCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const foodItemToAdd = {
+      id: foodItems.length + 1,
+      name: newFoodItem,
+    }
+    console.log(foodItemToAdd);
+    setFoodItems(foodItems.concat(foodItemToAdd));
+    setNewFoodItem('')
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={foodItemCreation}>
+          <input type="text" value={newFoodItem} onChange={e => setNewFoodItem(e.target.value)} />
+          <button type="submit">add</button>
+        </form>
+        <ul>
+          {foodItems.map(item =>
+            <li key={item.id}>{item.name}</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
